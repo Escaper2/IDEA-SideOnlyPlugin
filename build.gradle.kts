ea0c5d650bb4ea1e8db7110df92623fe14915c8f
Eskaper2 <vlad81552@gmail.com>
2023-04-03 20:10:35 +0300

    chore: project init

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("java")
+    id("org.jetbrains.intellij") version "1.10.1"
+}
+
+group = "escaper2.testTask"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+    version.set("2022.1.4")
+    type.set("IC") // Target IDE Platform
+
+    plugins.set(listOf("com.intellij.java"))
+}
+
+repositories {
+    mavenCentral()
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
+    }
+
+    patchPluginXml {
+        sinceBuild.set("221")
+        untilBuild.set("231.*")
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
