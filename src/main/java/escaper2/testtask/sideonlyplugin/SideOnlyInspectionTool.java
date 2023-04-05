cfb497b4e91c53a2d0598d36d059a3374529ba11
Eskaper2 <vlad81552@gmail.com>
2023-04-05 13:36:13 +0300

    feat: inspection feature for SideOnly annotation

--- src/main/java/escaper2/testtask/sideonlyplugin/SideOnlyInspectionTool.java
+++ src/main/java/escaper2/testtask/sideonlyplugin/SideOnlyInspectionTool.java
@@ -1,7 +1,106 @@
 package escaper2.testtask.sideonlyplugin;
 
 import com.intellij.codeInspection.*;
+import com.intellij.psi.*;
+import com.intellij.psi.util.PsiTreeUtil;
+import escaper2.testtask.sideonlyplugin.annotation.Side;
+import org.jetbrains.annotations.NotNull;
+
 
 public class SideOnlyInspectionTool extends AbstractBaseJavaLocalInspectionTool {
 
+    @NotNull
+    @Override
+    public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
+        return new JavaElementVisitor() {
+            @Override
+            public void visitReferenceExpression(PsiReferenceExpression expression) {
+                super.visitReferenceExpression(expression);
+                System.out.println(expression);
+                checkSideOnly(expression, holder);
+            }
+
+            @Override
+            public void visitMethod(PsiMethod method) {
+                super.visitMethod(method);
+                checkSideOnly(method, holder);
+            }
+
+            @Override
+            public void visitMethodCallExpression(PsiMethodCallExpression expression) {
+                super.visitMethodCallExpression(expression);
+                checkSideOnly(expression.getMethodExpression(), holder);
+            }
+
+            @Override
+            public void visitNewExpression(PsiNewExpression expression) {
+                super.visitNewExpression(expression);
+                checkSideOnly(expression.getClassReference(), holder);
+            }
+
+            private void checkSideOnly(PsiElement element, ProblemsHolder holder) {
+                if (element == null) return;
+                PsiElement resolved;
+                if (element instanceof PsiReference) {
+                    resolved = ((PsiReference) element).resolve();
+                } else {
+                    resolved = element;
+                }
+                if (resolved == null) return;
+                Side elementSide = getSide(resolved);
+                PsiMethod containingMethod = PsiTreeUtil.getParentOfType(element, PsiMethod.class);
+                if (containingMethod != null) {
+                    Side methodSide = getSide(containingMethod);
+                    elementSide = methodSide.compareSides(elementSide);
+                }
+                if (elementSide == Side.INVALID) {
+                    holder.registerProblem(element, "Invalid usage of @SideOnly");
+                }
+            }
+
+            private Side getSide(PsiElement element) {
+                Side side = Side.BOTH;
+                if (element instanceof PsiModifierListOwner) {
+                    PsiModifierList modifierList = ((PsiModifierListOwner) element).getModifierList();
+                    if (modifierList != null) {
+                        for (PsiAnnotation annotation : modifierList.getAnnotations()) {
+                            if (annotation.getQualifiedName().endsWith(".SideOnly")) {
+                                PsiAnnotationMemberValue value = annotation.findAttributeValue("value");
+                                if (value != null && value.getText().endsWith(".CLIENT")) {
+                                    side = Side.CLIENT;
+                                } else if (value != null && value.getText().endsWith(".SERVER")) {
+                                    side = Side.SERVER;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (element instanceof PsiClass) {
+                    PsiClass psiClass = (PsiClass) element;
+                    PsiClass superClass = psiClass.getSuperClass();
+                    side = side.compareSides(getSide(psiClass.getContainingClass()));
+
+                    if (superClass == null || superClass.getQualifiedName().equals("java.lang.Object")) return side;
+
+                    side = side.compareSides(getSide(psiClass.getSuperClass()));
+
+                    if (side == Side.INVALID) return Side.INVALID;
+
+                    for (PsiClass intf : psiClass.getInterfaces()) {
+                        side = side.compareSides(getSide(intf));
+                        if (side == Side.INVALID) return Side.INVALID;
+                    }
+
+                } else if (element instanceof PsiMethod) {
+                    return side.compareSides(getSide(((PsiMethod) element).getContainingClass()));
+
+                } else if (element instanceof PsiField) {
+                    return side.compareSides(getSide(((PsiField) element).getContainingClass()));
+                }
+
+                return side;
+            }
+        };
+    }
 }
+
--- src/main/java/escaper2/testtask/sideonlyplugin/annotation/Side.java
+++ src/main/java/escaper2/testtask/sideonlyplugin/annotation/Side.java
@@ -2,5 +2,17 @@ package escaper2.testtask.sideonlyplugin.annotation;
 
 public enum Side {
     CLIENT,
-    SERVER;
+    SERVER,
+    BOTH,
+    INVALID;
+
+    public Side compareSides(Side other) {
+        if (other == BOTH) return this;
+        if (this == BOTH) return other;
+
+        else if (this == INVALID || other == INVALID || this != other)
+            return INVALID;
+        else
+            return this;
+    }
 }
