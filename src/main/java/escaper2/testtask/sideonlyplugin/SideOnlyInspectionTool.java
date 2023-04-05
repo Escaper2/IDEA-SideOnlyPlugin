8685d38088cc6d2c3b2f14847d869700f3286347
Eskaper2 <vlad81552@gmail.com>
2023-04-05 16:39:21 +0300

    fix: fix inspection for interfaces and constructors

--- src/main/java/escaper2/testtask/sideonlyplugin/SideOnlyInspectionTool.java
+++ src/main/java/escaper2/testtask/sideonlyplugin/SideOnlyInspectionTool.java
@@ -35,7 +35,13 @@ public class SideOnlyInspectionTool extends AbstractBaseJavaLocalInspectionTool 
             @Override
             public void visitNewExpression(PsiNewExpression expression) {
                 super.visitNewExpression(expression);
-                checkSideOnly(expression.getClassReference(), holder);
+                PsiMethod constructor = expression.resolveConstructor();
+                if (constructor != null) {
+                    checkSideForConstructor(expression.getClassReference(), constructor, holder);
+
+                } else {
+                    checkSideOnly(expression.getClassReference(), holder);
+                }
             }
 
             private void checkSideOnly(PsiElement element, ProblemsHolder holder) {
@@ -54,8 +60,30 @@ public class SideOnlyInspectionTool extends AbstractBaseJavaLocalInspectionTool 
                     elementSide = methodSide.compareSides(elementSide);
                 }
                 if (elementSide == Side.INVALID) {
-                    holder.registerProblem(element, "Invalid usage of @SideOnly");
+                    holder.registerProblem(element, "Can not access side-only " + element + " from here");
                 }
+            }
+
+            private void checkSideForConstructor(PsiElement element, PsiMethod constructor, ProblemsHolder holder) {
+                if (element == null) return;
+                PsiElement resolved;
+                if (element instanceof PsiReference) {
+                    resolved = ((PsiReference) element).resolve();
+                } else {
+                    resolved = element;
+                }
+                if (resolved == null) return;
+                Side elementSide = getSide(resolved);
+                Side constructorSide = getSide(constructor);
+                PsiMethod containingMethod = PsiTreeUtil.getParentOfType(element, PsiMethod.class);
+                if (containingMethod != null) {
+                    Side methodSide = getSide(containingMethod);
+                    elementSide = methodSide.compareSides(constructorSide);
+                }
+                if (elementSide == Side.INVALID) {
+                    holder.registerProblem(element, "Can not access side-only  " + element + " from here");
+                }
+
             }
 
             private Side getSide(PsiElement element) {
@@ -80,16 +108,17 @@ public class SideOnlyInspectionTool extends AbstractBaseJavaLocalInspectionTool 
                     PsiClass superClass = psiClass.getSuperClass();
                     side = side.compareSides(getSide(psiClass.getContainingClass()));
 
-                    if (superClass == null || superClass.getQualifiedName().equals("java.lang.Object")) return side;
-
-                    side = side.compareSides(getSide(psiClass.getSuperClass()));
-
-                    if (side == Side.INVALID) return Side.INVALID;
-
                     for (PsiClass intf : psiClass.getInterfaces()) {
                         side = side.compareSides(getSide(intf));
                         if (side == Side.INVALID) return Side.INVALID;
                     }
+
+                    if (side == Side.INVALID) return Side.INVALID;
+
+                    if (superClass == null || superClass.getQualifiedName().equals("java.lang.Object")) return side;
+
+                    side = side.compareSides(getSide(psiClass.getSuperClass()));
+
 
                 } else if (element instanceof PsiMethod) {
                     return side.compareSides(getSide(((PsiMethod) element).getContainingClass()));
